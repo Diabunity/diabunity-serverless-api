f5c5666e50a4d4c6382f0bf558d55afabf44c026
Ari Gonzalez <ariel.damian.gonzalez@gmail.com>
2023-02-11 23:58:59 -0300

    Add serveStatic middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono';
+import { serveStatic } from 'hono/serve-static';
 
 const app = new Hono();
 
 app.get('/', (c) => {
   return c.text('Hello World!');
 });
+
+app.get('/static/*', serveStatic({ root: './' }));
 
 export default app;
